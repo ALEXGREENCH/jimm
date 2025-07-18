655426d22216e8b5ffd6f5bc691df743b9d752ca
ALEXGREENCH <greench4alex@gmail.com>
2025-07-19 00:21:28 +0300

    Add Gradle build support

---
+++ stubs/com/motorola/io/FileSystemRegistry.java
@@ -0,0 +1,4 @@
+package com.motorola.io;
+public class FileSystemRegistry {
+    public static String[] listRoots() { return new String[0]; }
+}
---
+++ stubs/com/samsung/util/LCDLight.java
@@ -0,0 +1,5 @@
+package com.samsung.util;
+public class LCDLight {
+    public static void on(int duration) {}
+    public static void off() {}
+}
